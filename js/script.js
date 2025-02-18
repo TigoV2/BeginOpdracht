f04b0b695b1284698ccbe1050d32477c10d1897c
TigoV2 <102575@glr.nl>
2025-02-18 13:42:49 +0100

    broken image loader later fixen

--- js/script.js
+++ js/script.js
@@ -1,25 +1,16 @@
 const pictures= [
-    [
-        "../images/composition.jpeg",
-    ],
-    [
-        "../images/arabian_moucharabieh.jpeg",
-        "../images/soleil-oblique.jpeg",
-    ]
+        "../images/mainphoto.jpg",
+        "../images/contact.png",
+        "../images/contacten.png",
 ];
 
 function loadImages() {
     for (let index = 0; index < pictures.length; index++) {
-        let images = pictures[index];
-        
-        for (let img = 0; img < images.length; img++) {
-            let image = document.createElement("img");
-            image.src = images[img];
+            let img = document.createElement("img");
+            img.src = pictures[img];
 
-            const item = index * 2 + (img + 1);
             document.getElementById(`picture-${item}`).appendChild(img);
         }
     }
-}
 
 window.onload = loadImages;
