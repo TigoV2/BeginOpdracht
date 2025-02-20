14e66a98202606e3e426bc8534e204cf1b96de5d
TigoV2 <102575@glr.nl>
2025-02-20 20:50:55 +0100

    Meeste pagina's af (niet top-quality)

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ const pictures= [
 ];
 
 function loadImages() {
-    for (let index = 0; index < pictures.length; index++) {
-            let img = document.createElement("img");
-            img.src = pictures[index];
-
-            document.getElementById(`picture-${index + 1}`).appendChild(img);
-        }
+  for (let index = 0; index < pictures.length; index++) {
+    let img = document.createElement("img");
+    img.src = pictures[index];
+    
+    document.getElementById(`picture-${index + 1}`).appendChild(img);
     }
+}
 
 window.onload = loadImages;
 
